ff064364ce60edf64990a5de2dc1a2f5409a4d84
Gutovysk <gutovysk@gmail.com>
2021-11-05 23:00:54 -0300

    prova social aparecendo na tela; novos volumes para venda na página de vendas; 321 imagens prova social

---
+++ assets-manual/js/prova-social.js
@@ -0,0 +1,39 @@
+$( document ).ready(function() {
+
+        var myTime;
+        var texto;
+        var tempo = 8000;
+        var c = 0;
+        var clique = 0;
+        var path = 'assets-manual/files/';
+        var qtd_files = 321;
+
+        function get_novo_depoimento() {
+          c=c+1;
+          texto="TOAST "+c;
+          tempo=Math.round(Math.random() * 10 + 13)*1000;
+          if (c>3) {
+            c=0;
+            tempo=30000;
+            clique=0;
+          };
+          i = Math.floor(Math.random()*qtd_files+1);
+          var url = (path+i+".png");
+          $('#img-toast').attr('src', url);
+        }
+
+        var show_toast = function() {
+            get_novo_depoimento();
+            //$("#tempo").text("ordem "+c+" tempo = "+tempo+" clic = "+clique);
+            $('#liveToast').toast('show');
+            myTime = setTimeout(show_toast, tempo);
+        }
+        myTime = setTimeout(show_toast, tempo);
+
+        $( "#liveToastBtn" ).click(function() {
+            clearTimeout(myTime);
+            myTime = setTimeout(show_toast, clique*1000);
+            clique+=0.25;
+        });
+
+     });
