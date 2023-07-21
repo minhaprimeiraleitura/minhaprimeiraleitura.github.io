5fa9fdb1a30c3ad06e6f2ce0deeb2abd5123ac45
Gutovysk <gutovysk@gmail.com>
2023-07-20 22:28:42 -0300

    inserindo carrossel das crianças lendo na página de venda usando flickity; site do carrossel: https://flickity.metafizzy.co/; library do carrossel e do slick

---
+++ assets-manual/js/conf-carrossel-index.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+      $('.carousel-childs-reading').slick({
+        dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  centerMode: true,
+  variableWidth: true
+      });
+});
